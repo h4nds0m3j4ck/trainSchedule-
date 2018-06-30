0f302170b5ac902a277d5c8de9c1c14a4328c543
h4nds0m3j4ck <estrella.terenzio@gmail.com>
2018-06-30 11:52:36 -0400

    added some stuff

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+// Firebase 
+var config = {
+    apiKey: "AIzaSyAagiozHVaxmsjUTaIigyVgJiNMSwEc2OU",
+    authDomain: "train-schedule-232a8.firebaseapp.com",
+    databaseURL: "https://train-schedule-232a8.firebaseio.com",
+    projectId: "train-schedule-232a8",
+    storageBucket: "train-schedule-232a8.appspot.com",
+    messagingSenderId: "220296618015"
+  };
+  firebase.initializeApp(config);
